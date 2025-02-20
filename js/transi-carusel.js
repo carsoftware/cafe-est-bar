09482b5c15fdde0b185bd78983d5f063ab5a03ac
carlosruiz <carlos288913@gmail.com>
2025-02-19 22:30:39 -0500

    Insercion de carusel y slogan con iconos reservas,promociones y carrito de ocmpras

---
+++ js/transi-carusel.js
@@ -0,0 +1,6 @@
+// Iniciar el carousel con la configuración de transición
+const carousel = new bootstrap.Carousel(document.querySelector('#imageCarousel'), {
+    interval: 5000,  // Cambiar imagen cada 5 segundos
+    ride: 'carousel'
+  });
+  
